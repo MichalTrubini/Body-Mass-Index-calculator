fad47f33ffd777476bfc609dba5ba38ff27c2415
Michal Trubini <mchl.trubini@gmail.com>
2023-10-08 11:26:55 +0200

    tailwind settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        "blue": "#345FF6",
+        "gunmetal": "#253347",
+        "darkGrey": "#5E6E85",
+        "medium-grey": "#7C8798",
+      },
+      textColor: {
+      },
+      screens: {
+      },
+      fontSize: {
+      },
+      maxWidth: {
+      }
+    },
   },
   plugins: [],
 }
